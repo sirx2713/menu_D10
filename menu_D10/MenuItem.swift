9404c01f0671ff73c635a503a1170c5b23148bde
sirx2713 <bradx2713@gmail.com>
2024-03-13 03:23:50 +0300

    10 Attempts of Making the Menu Application.

---
+++ menu_D10/MenuItem.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItem.swift
+//  menu_D10
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable {
+    
+    var id: UUID = UUID()
+    var name:String
+    var price:String
+    var imageName:String
+    
+    
+}
